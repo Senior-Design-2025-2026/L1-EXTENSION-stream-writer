1985e49dde0488351db8e2a332a4f9af91517db5
mattnkrueger <mkrue138@gmail.com>
2025-09-29 12:12:44 -0500

    virtualization for dummy streamer

--- src/dummy/dummy_writer.py
+++ src/dummy/dummy_writer.py
@@ -2,36 +2,42 @@ import time
 import random
 import redis
 
+from src.dummy.virtualization import dummy_check_button_toggle
 from src.setup.task_queue import celery_client
+from src.setup.redis_client import r
+
+def dummy_stream_record(sensor_id:str, timestamp:int):
+    temp = random.uniform(25,35)
+    entry = {
+        "sensor_id":sensor_id,
+        "temperature_c":f"{temp:.2f}",
+    }
+
+    # check for toggle
+    status = dummy_check_button_toggle(sensor_id=sensor_id)
+
+    # write if sensor is "ON"
+    if status == "ON":
+        r.xadd("readings", entry, id=f"{timestamp}-{sensor_id}", maxlen=300)
+
+        celery_client.send_task(
+            "insert_record", 
+            kwargs={
+                "sensor_id":sensor_id,
+                "timestamp":timestamp,
+                "temperature_c":temp
+            }
+        )
 
 def dummy_writer(r, celery_client):
+    # SETUP - start both sensors on
+    r.set("virtual:1:status", "ON")
+    r.set("virtual:2:status", "ON")
+
     while True:
-        t = int(time.time())
-        temp_1 = random.uniform(15,45)
-        entry_1 = {"sensor_id": "1", "temperature_c": f"{temp_1:.2f}"}
+        timestamp = int(time.time())
 
-        temp_2 = random.uniform(15,45)
-        entry_2 = {"sensor_id": "2", "temperature_c": f"{temp_2:.2f}"}
-
-        r.xadd("readings", entry_1, id=f"{t}-1", maxlen=300)
-        r.xadd("readings", entry_2, id=f"{t}-2", maxlen=300)
+        dummy_stream_record(sensor_id="1", timestamp=timestamp)
+        dummy_stream_record(sensor_id="2", timestamp=timestamp)
             
-        celery_client.send_task(
-            "insert_record", 
-            kwargs={
-                "sensor_id":1,
-                "timestamp":t,
-                "temperature_c":temp_1
-            }
-        )
-
-        celery_client.send_task(
-            "insert_record", 
-            kwargs={
-                "sensor_id":2,
-                "timestamp":t,
-                "temperature_c":temp_2
-            }
-        )
-
         time.sleep(1)
